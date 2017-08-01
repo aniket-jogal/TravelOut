e01179d7cd2e261016a26d045b35991018afc93c
aniket-jogal <aniket.jogal1090@gmail.com>
2017-08-01 16:33:11 +0530

    Fifth Commit

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@
 var angular =require('angular');
 require('angular-route');
 
+require('./controller');
 var app=angular.module('TravelOutApp',['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider.when('',{
